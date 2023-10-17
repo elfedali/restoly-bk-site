dd47d79c89c52a31997d05042c2839af8e3a5fcb
Abdessamad EL FEDALI <a.elfedali@gmail.com>
2023-10-17 13:59:24 +0100

    Register & login

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordHasherInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('webmaster');
+        $user->setPassword($this->passwordEncoder->hashPassword(
+            $user,
+            'password'
+        ));
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
